df79659835970cdd0fac3f6b9fc0f218f1ce9da0
Cella Sum <cella.sum@gmail.com>
2019-01-28 23:34:57 -0500

    Add High Scores Exercise

---
+++ high-scores/high_scores.rb
@@ -0,0 +1,24 @@
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    scores.last
+  end
+
+  def personal_best
+    scores.max
+  end
+
+  def personal_top
+    scores.sort.reverse.take(3)
+  end
+
+  def report
+    difference = "#{personal_best - latest} short of" if personal_best > latest
+    "Your latest score was #{latest}. That's #{difference} your personal best!".squeeze
+  end
+end
